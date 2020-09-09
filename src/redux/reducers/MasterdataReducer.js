244ca0a9e545b3713b2fa243252ffd6c862f61cb
alifsabrani <alifsabrani@gmail.com>
2020-09-09 10:38:57 +0800

    add .vscode folder to .gitignore

--- src/redux/reducers/MasterdataReducer.js
+++ src/redux/reducers/MasterdataReducer.js
@@ -8,6 +8,17 @@ export default function reducer(state = defaultState, action) {
             return Object.assign({}, state, {
                 product: action.data
             });
+            case 'FETCH_COMPANY':
+                return { ...state, data: {}, isFetching: true };
+              case 'FETCH_COMPANY_SUCCESS':
+                return {
+                  ...state,
+                  data: action.payload,
+                  isFetching: false,
+                  errors: false
+                };
+              case 'FETCH_COMPANY_FAILED':
+                return { ...state, isFetching: false, errors: true };
         default:
             return state;
     }
