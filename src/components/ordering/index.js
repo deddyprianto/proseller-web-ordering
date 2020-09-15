d0a2b1880c29fd54fef342862187177cdfe64e71
Crs Sulton <chaerussulton@gmail.com>
2020-09-15 12:02:08 +0800

    fix fetchDefaultOutlet

--- src/components/ordering/index.js
+++ src/components/ordering/index.js
@@ -49,11 +49,26 @@ class Ordering extends Component {
       isEmenu ? this.handleScrollEmenu : this.handleScrollWebOrdering
     );
 
-    if (!window.location.pathname.includes("emenu")) await this.props.dispatch(OutletAction.fetchDefaultOutlet());
+    if (!this.getUrlParameters() || !window.location.pathname.includes("emenu"))
+      await this.props.dispatch(OutletAction.fetchDefaultOutlet());
 
     await this.props.dispatch(OrderAction.getCart());
     await this.setState({ defaultOutlet: this.props.defaultOutlet });
     this.fetchCategories(this.props.defaultOutlet);
+  };
+
+  getUrlParameters = (pageParamString = null) => {
+    if (!pageParamString) pageParamString = window.location.href.split("?")[1];
+    if (pageParamString) {
+      var paramsArray = pageParamString.split("&");
+      var paramsHash = {};
+
+      for (var i = 0; i < paramsArray.length; i++) {
+        var singleParam = paramsArray[i].split("=");
+        paramsHash[singleParam[0]] = singleParam[1];
+      }
+      return paramsHash;
+    }
   };
 
   componentWillUnmount() {
@@ -81,7 +96,7 @@ class Ordering extends Component {
           header.style.top = 0;
         }
       }
-    } catch (e) {}
+    } catch (e) { }
   };
 
   handleScrollEmenu = (e) => {
@@ -98,7 +113,7 @@ class Ordering extends Component {
           searchButton.classList.add("search-button-absolute");
         }
       }
-    } catch (e) {}
+    } catch (e) { }
   };
 
   fetchCategories = async (outlet) => {
@@ -203,7 +218,7 @@ class Ordering extends Component {
             }
           });
       });
-    } catch (e) {}
+    } catch (e) { }
 
     if (isEmptyObject(basket)) {
       product.quantity = 1;
@@ -325,7 +340,7 @@ class Ordering extends Component {
               items.push(productsBackup[i].items[j]);
             }
           }
-        } catch (e) {}
+        } catch (e) { }
 
         if (items.length != 0) {
           if (productsSearch == undefined) {
@@ -343,7 +358,7 @@ class Ordering extends Component {
 
       await this.setState({ products: productsSearch });
       await this.setState({ loading: false, loadingSearching: false });
-    } catch (e) {}
+    } catch (e) { }
   };
 
   loadingSearching = async (loadingSearching) => {
@@ -451,15 +466,15 @@ class Ordering extends Component {
             selectedCategory={this.state.selectedCategory}
           />
         ) : (
-          <CategoriesWebOrdering
-            loadingSearching={this.loadingSearching}
-            finished={finished}
-            setLoading={this.setLoading}
-            searchProduct={this.searchProduct}
-            categories={categories}
-            selectedCategory={this.state.selectedCategory}
-          />
-        )}
+            <CategoriesWebOrdering
+              loadingSearching={this.loadingSearching}
+              finished={finished}
+              setLoading={this.setLoading}
+              searchProduct={this.searchProduct}
+              categories={categories}
+              selectedCategory={this.state.selectedCategory}
+            />
+          )}
         <div
           className="full-width list-view columns-2 archive woocommerce-page html-change"
           style={{ marginTop: isEmenu ? 35 : 5 }}
