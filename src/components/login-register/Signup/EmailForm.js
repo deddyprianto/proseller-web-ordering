dcd8cc477818f6212059a534ffd4fd205c4d337b
alifsabrani <alifsabrani@gmail.com>
2020-10-01 11:36:56 +0800

    Show custom fields when user sign up using email

--- src/components/login-register/Signup/EmailForm.js
+++ src/components/login-register/Signup/EmailForm.js
@@ -14,6 +14,7 @@ const EmailForm = ({
   errorPassword,
   enablePassword,
   error,
+  children,
 }) => {
   const initialCountry = "SG";
   const [phoneCountryCode, setPhoneCountryCode] = useState("+65");
@@ -78,6 +79,7 @@ const EmailForm = ({
           {error}
         </div>
       )}
+      {children}
       {enablePassword && (
         <PasswordField
           handleChange={handleChange}
