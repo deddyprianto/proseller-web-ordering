49601686a41ea1c64484c6b379c7f32814428cc0
alifsabrani <alifsabrani@gmail.com>
2020-12-04 18:38:36 +0800

    Fix issue : can't register with email that include uppercase character

--- src/components/login-register/Signup/EmailForm.js
+++ src/components/login-register/Signup/EmailForm.js
@@ -173,7 +173,7 @@ const EmailForm = ({
         </>
       )}
       <Button
-        disabled={isSubmitting}
+        disabled={isSubmitting || agreeTC || !isTCAvailable}
         className="button"
         style={{ width: "100%", marginTop: 10, borderRadius: 5, height: 50 }}
         onClick={() => handleSubmit()}
