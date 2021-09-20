5d11adfea3ff0e47a1d14f721c6be1967607ad64
troykawengian77 <88697640+destryo.kawengian@users.noreply.github.com>
2021-09-20 09:41:30 +0700

    update fix popejai
    fixing issue

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -154,7 +154,7 @@ export class MapContainer extends Component {
       <div>
         <div
           style={{
-            marginTop: 90,
+            marginTop: 100,
             marginLeft: 10,
             marginRight: 10,
             display: "flex",
