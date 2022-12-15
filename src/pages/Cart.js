15f814444169328d744fe648b5ef5aed34069187
deddyps <haicoders12@gmail.com>
2022-12-15 15:33:00 +0700

    add new again

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -796,7 +796,7 @@ const Cart = ({ ...props }) => {
 
     await props.dispatch({
       type: 'SET_SELECTED_DELIVERY_PROVIDERS',
-      payload: value,
+      data: value,
     });
 
     const response = await props.dispatch(
