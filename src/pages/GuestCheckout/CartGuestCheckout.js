887fa9084b66f46791db44bdda0cc7366d690719
iqbaldp <miqballdp@gmail.com>
2022-12-26 13:13:57 +0800

    fix guest checkout email validation expression

--- src/pages/GuestCheckout/CartGuestCheckout.js
+++ src/pages/GuestCheckout/CartGuestCheckout.js
@@ -359,10 +359,9 @@ const CartGuestCheckout = () => {
       });
     },
   });
-  const validateEmailRegex =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  const validateEmailRegex = /^[\w-\.+]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-  let email = formik.values.email;
+  let email = formik.values.email?.toLowerCase();
   const formRegexMail = validateEmailRegex.test(email);
 
   const handlePaymentGuestMode = async () => {
@@ -2080,7 +2079,7 @@ const CartGuestCheckout = () => {
                   paddingBottom: '2rem',
                   marginBottom: '0.5rem',
                 }}
-                value={formik.values.email || ''}
+                value={formik.values?.email?.toLowerCase() || ''}
                 size='small'
                 placeholder='Your Email'
                 onChange={formik.handleChange}
@@ -2357,7 +2356,7 @@ const CartGuestCheckout = () => {
                   paddingBottom: '2rem',
                   marginBottom: '0.5rem',
                 }}
-                value={formik.values.email || ''}
+                value={formik.values?.email?.toLowerCase() || ''}
                 size='small'
                 placeholder='Your Email'
                 onChange={formik.handleChange}
