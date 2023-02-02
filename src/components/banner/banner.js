92a89b76f613ece96b1e8c96157e06082750c884
deddyps <haicoders12@gmail.com>
2023-02-02 21:04:56 +0700

    add else if to condition if url link exist and setup from admin portal

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -3,7 +3,8 @@ import { isEmptyArray } from '../../helpers/CheckEmpty';
 
 import { MasterDataService } from '../../Services/MasterDataService';
 import { useSelector } from 'react-redux';
-
+import { Link as LinkRouter } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
 
@@ -37,9 +38,14 @@ const Banner = () => {
   if (isEmptyArray(banners)) return null;
 
   const renderBannerItem = (item) => {
-    if (item?.linkBanner) {
+    if (item?.promotionId) {
+      const promotionId = item.promotionId.split(':')[2];
       return (
-        <a href={item?.url} target='_blank' rel='noreferrer'>
+        <Link
+          component={LinkRouter}
+          to={`/promotions-detail/${promotionId}`}
+          rel='noopener noreferrer'
+        >
           <img
             src={item.defaultImageURL}
             alt={item.name}
@@ -49,6 +55,12 @@ const Banner = () => {
               borderRadius: 10,
             }}
           />
+        </Link>
+      );
+    } else if (item?.url) {
+      return (
+        <a href={item.url} target='_blank' rel='noopener noreferrer'>
+          <img src={item.defaultImageURL} alt={item.name} width='100%' />
         </a>
       );
     } else {
