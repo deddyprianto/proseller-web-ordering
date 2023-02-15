37e1de40ad05baea512cdfab07bd313d60c46d13
deddyps <haicoders12@gmail.com>
2023-02-15 09:44:37 +0700

    WIP

--- src/redux/actions/MasterDataAction.js
+++ src/redux/actions/MasterDataAction.js
@@ -116,7 +116,7 @@ function getDomainName() {
     const domainName =
       window.location.hostname !== 'localhost'
         ? window.location.hostname
-        : 'ordering-pink-city.proseller-demo.com';
+        : 'ordering-chickyfun.proseller-demo.com';
     try {
       fetch(process.env.REACT_APP_DOMAIN_MAPPING_URL, {
         method: 'POST',
