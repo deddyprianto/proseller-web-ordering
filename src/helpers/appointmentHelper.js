3ee307ed841dc17f946da3f75a0a79594d84f80c
deddyps <haicoders12@gmail.com>
2023-08-21 12:12:01 +0700

    fix: PROS-2986 time duration issue

--- src/helpers/appointmentHelper.js
+++ src/helpers/appointmentHelper.js
@@ -8,7 +8,7 @@ export const convertTimeToStr = (seconds) => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   if (hours > 0) {
-    return minutes > 0 ? `${hours}h ${minutes}mins` : '60mins';
+    return minutes > 0 ? `${hours}h ${minutes}mins` : `${hours}hours`;
   } else if (minutes > 0) {
     return `${minutes}mins`;
   } else {
