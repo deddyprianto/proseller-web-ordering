d91e7fd9605802028e0e363241899627b08d368d
deddyps <haicoders12@gmail.com>
2024-05-24 09:44:48 +0700

    change the masterdataAction file to default

--- src/redux/actions/MasterDataAction.js
+++ src/redux/actions/MasterDataAction.js
@@ -114,9 +114,9 @@ function setDefaultOutlet(outlet) {
 function getDomainName() {
   return async (dispatch) => {
     const domainName =
-      window.location.hostname !== "localhost"
+      window.location.hostname !== 'localhost'
         ? window.location.hostname
-        : "ordering-pink-city.proseller-demo.com";
+        : process.env.REACT_APP_COMPANY_URL;
     try {
       fetch(process.env.REACT_APP_DOMAIN_MAPPING_URL, {
         method: 'POST',
